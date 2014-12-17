419f90019cce8d25f818ae6613f46d7b98c4f36f
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2014-12-17 13:58:09 +0100

    rename trail to trace

--- src/SequenceAlignment/Gotoh.fs
+++ src/SequenceAlignment/Gotoh.fs
@@ -2,15 +2,15 @@
 
 open System
 
-type Trail<'a> = Trail of ArrayCell<'a>[,] * int * int
-and ArrayCell<'a> = 'a * option<Trail<'a>>
+type Trace<'a> = Trace of ArrayCell<'a>[,] * int * int
+and ArrayCell<'a> = 'a * option<Trace<'a>>
 type AlignmentCell = ArrayCell<Alignment>
 
 let highest(arrays: list<ArrayCell<'a>[,]>, i, j) : ArrayCell<'a> =
     arrays
     |> List.map (fun a -> fst a.[i,j], a)
     |> List.maxBy fst
-    |> (fun (value,array) -> value, Some(Trail (array,i,j)))
+    |> (fun (value,array) -> value, Some(Trace (array,i,j)))
 
 let inline addFst x (f,s) = f + x, s
 
@@ -46,15 +46,15 @@ let run
         |> List.map (fun k -> highest([a;c], k, j) |> addFst (p(i-k)))
         |> List.maxBy fst
     let countC(i,j) : AlignmentCell =
-        fst s.[i-1,j-1] + sim(fstSeq.[i-1], sndSeq.[j-1]), Some(Trail(c,i,j))
+        fst s.[i-1,j-1] + sim(fstSeq.[i-1], sndSeq.[j-1]), Some(Trace(c,i,j))
 
     s.[0,0] <- 0., None
     for i in 1..len1 do 
         b.[i,0] <- p(i), None
-        s.[i,0] <- p(i), Some(Trail(b,i,0))
+        s.[i,0] <- p(i), Some(Trace(b,i,0))
     for i in 1..len2 do
         a.[0,i] <- p(i), None
-        s.[0,i] <- p(i), Some(Trail(a,0,i))
+        s.[0,i] <- p(i), Some(Trace(a,0,i))
     for i in 1..len1 do
         for j in 1..len2 do
             a.[i,j] <- countA(i,j)
@@ -63,22 +63,22 @@ let run
             s.[i,j] <- highest([a;b;c],i,j)
             printState(a,b,c,s)
 
-    let rec unfoldTrail (acc,trail : option<Trail<Alignment>>) =
+    let rec traceBack (acc,trail : option<Trace<Alignment>>) =
         match trail with
-        | Some(Trail(array,i,j)) when array.Equals(a) -> 
+        | Some(Trace(array,i,j)) when array.Equals(a) -> 
             let nextTrail = snd a.[i,j]
-            let from = match nextTrail with Some(Trail(_,_,j)) -> j+1 | None -> 1
+            let from = match nextTrail with Some(Trace(_,_,j)) -> j+1 | None -> 1
             let indels = [from..j] |> List.map (fun j -> Break, Nucl sndSeq.[j-1])
-            unfoldTrail (indels @ acc, nextTrail)
-        | Some(Trail(array,i,j)) when array.Equals(b) -> 
+            traceBack (indels @ acc, nextTrail)
+        | Some(Trace(array,i,j)) when array.Equals(b) -> 
             let nextTrail = snd b.[i,j]
-            let from = match nextTrail with Some(Trail(_,i,_)) -> i+1 | None -> 1
+            let from = match nextTrail with Some(Trace(_,i,_)) -> i+1 | None -> 1
             let indels = [from..i] |> List.map (fun i -> Nucl fstSeq.[i-1], Break)
-            unfoldTrail (indels @ acc, nextTrail)
-        | Some(Trail(array,i,j)) when array.Equals(c) -> 
-            unfoldTrail ((Nucl fstSeq.[i-1],Nucl sndSeq.[j-1])::acc, snd s.[i-1,j-1])
+            traceBack (indels @ acc, nextTrail)
+        | Some(Trace(array,i,j)) when array.Equals(c) -> 
+            traceBack ((Nucl fstSeq.[i-1],Nucl sndSeq.[j-1])::acc, snd s.[i-1,j-1])
         | None -> acc
         | _ -> failwith "unexpected trail"
     
-    let sequence = unfoldTrail([], snd s.[len1,len2])
+    let sequence = traceBack([], snd s.[len1,len2])
     fst s.[len1,len2], sequence
